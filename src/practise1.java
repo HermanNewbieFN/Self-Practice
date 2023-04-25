95775f478baf7989a47dca844bd8a85f6cc63ac0
Artyom Rezkin <rezksn@mail.ru>
2023-04-26 00:16:59 +0300

    return statement in Java2

--- src/practise1.java
+++ src/practise1.java
@@ -1,13 +1,18 @@
 
 
-public class Main {
+public class practise1 {
     public static void main(String[] args) {
     double sum;
     sum= simpleMath(974.2, 123.1);
         System.out.println(sum);
+        String frmBtolow = lowcase("USING METHODE STRING LOWER CASE");
+        System.out.println(frmBtolow);
     }
     public static double simpleMath(double a, double b){
     return a + b;
 
+    }
+    public static String lowcase (String l){
+        return l.toLowerCase();
     }
 }
