9e496f9dce2ec654c653c6de4b731e9cc527d256
Artyom Rezkin <rezksn@mail.ru>
2023-04-25 23:51:11 +0300

    return statement in Java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+    double sum;
+    sum= simpleMath(974.2, 123.1);
+        System.out.println(sum);
+    }
+    public static double simpleMath(double a, double b){
+    return a + b;
+
+    }
+}
